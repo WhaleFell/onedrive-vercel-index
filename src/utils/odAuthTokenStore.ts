1b0827c67f23a3c06bb9d83b850494d3f7f939f5
WhaleFell <whalefall9420@outlook.com>
2023-08-18 16:06:12 +0800

    🐞 fix: session

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -5,53 +5,9 @@ import siteConfig from '../../config/site.config'
 // // https://vercel.com/integrations/upstash
 const kv = new Redis(process.env.REDIS_URL || '')
 
-// export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-//   const accessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
-//   const refreshToken = await kv.get(`${siteConfig.kvPrefix}refresh_token`)
-
-//   return {
-//     accessToken,
-//     refreshToken,
-//   }
-// }
-
-// export async function storeOdAuthTokens({
-//   accessToken,
-//   accessTokenExpiry,
-//   refreshToken,
-// }: {
-//   accessToken: string
-//   accessTokenExpiry: number
-//   refreshToken: string
-// }): Promise<void> {
-//   await kv.set(`${siteConfig.kvPrefix}access_token`, accessToken, 'EX', accessTokenExpiry)
-//   await kv.set(`${siteConfig.kvPrefix}refresh_token`, refreshToken)
-// }
-
-
-// store token in browser session
-
-// 从 Redis 获取访问令牌和刷新令牌，并缓存到浏览器会话中
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-  // 尝试从浏览器会话中获取缓存的令牌
-  const cachedAccessToken = sessionStorage.getItem('accessToken')
-  const cachedRefreshToken = sessionStorage.getItem('refreshToken')
-
-  // 如果缓存存在，则直接返回缓存的令牌
-  if (cachedAccessToken && cachedRefreshToken) {
-    return {
-      accessToken: cachedAccessToken,
-      refreshToken: cachedRefreshToken,
-    }
-  }
-
-  // 如果缓存不存在，则从 Redis 获取令牌
   const accessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
   const refreshToken = await kv.get(`${siteConfig.kvPrefix}refresh_token`)
-
-  // 将获取的令牌缓存到浏览器会话中
-  sessionStorage.setItem('accessToken', accessToken || '') // 使用空字符串作为默认值
-  sessionStorage.setItem('refreshToken', refreshToken || '') // 使用空字符串作为默认值
 
   return {
     accessToken,
@@ -59,7 +15,6 @@ export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refresh
   }
 }
 
-// 将访问令牌和刷新令牌存储到 Redis，并更新浏览器会话中的缓存
 export async function storeOdAuthTokens({
   accessToken,
   accessTokenExpiry,
@@ -69,11 +24,56 @@ export async function storeOdAuthTokens({
   accessTokenExpiry: number
   refreshToken: string
 }): Promise<void> {
-  // 将令牌存储到 Redis
   await kv.set(`${siteConfig.kvPrefix}access_token`, accessToken, 'EX', accessTokenExpiry)
   await kv.set(`${siteConfig.kvPrefix}refresh_token`, refreshToken)
-
-  // 更新浏览器会话中的缓存
-  sessionStorage.setItem('accessToken', accessToken)
-  sessionStorage.setItem('refreshToken', refreshToken)
 }
+
+
+// store token in browser session
+
+// 从 Redis 获取访问令牌和刷新令牌，并缓存到浏览器会话中
+// export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
+//   // 尝试从浏览器会话中获取缓存的令牌
+//   const cachedAccessToken = sessionStorage.getItem('accessToken')
+//   const cachedRefreshToken = sessionStorage.getItem('refreshToken')
+
+//   // 如果缓存存在，则直接返回缓存的令牌
+//   if (cachedAccessToken && cachedRefreshToken) {
+//     return {
+//       accessToken: cachedAccessToken,
+//       refreshToken: cachedRefreshToken,
+//     }
+//   }
+
+//   // 如果缓存不存在，则从 Redis 获取令牌
+//   const accessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
+//   const refreshToken = await kv.get(`${siteConfig.kvPrefix}refresh_token`)
+
+//   // 将获取的令牌缓存到浏览器会话中
+//   sessionStorage.setItem('accessToken', accessToken || '') // 使用空字符串作为默认值
+//   sessionStorage.setItem('refreshToken', refreshToken || '') // 使用空字符串作为默认值
+
+//   return {
+//     accessToken,
+//     refreshToken,
+//   }
+// }
+
+// // 将访问令牌和刷新令牌存储到 Redis，并更新浏览器会话中的缓存
+// export async function storeOdAuthTokens({
+//   accessToken,
+//   accessTokenExpiry,
+//   refreshToken,
+// }: {
+//   accessToken: string
+//   accessTokenExpiry: number
+//   refreshToken: string
+// }): Promise<void> {
+//   // 将令牌存储到 Redis
+//   await kv.set(`${siteConfig.kvPrefix}access_token`, accessToken, 'EX', accessTokenExpiry)
+//   await kv.set(`${siteConfig.kvPrefix}refresh_token`, refreshToken)
+
+//   // 更新浏览器会话中的缓存
+//   sessionStorage.setItem('accessToken', accessToken)
+//   sessionStorage.setItem('refreshToken', refreshToken)
+// }
