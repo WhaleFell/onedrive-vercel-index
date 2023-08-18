eef825d1c110b95c38731473e6aecc8f54073160
WhaleFell <whalefall9420@outlook.com>
2023-08-18 15:26:47 +0800

    🎈 perf: store token in browser

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -1,13 +1,56 @@
 import Redis from 'ioredis'
 import siteConfig from '../../config/site.config'
 
-// Persistent key-value store is provided by Redis, hosted on Upstash
-// https://vercel.com/integrations/upstash
+// // Persistent key-value store is provided by Redis, hosted on Upstash
+// // https://vercel.com/integrations/upstash
 const kv = new Redis(process.env.REDIS_URL || '')
 
+// export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
+//   const accessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
+//   const refreshToken = await kv.get(`${siteConfig.kvPrefix}refresh_token`)
+
+//   return {
+//     accessToken,
+//     refreshToken,
+//   }
+// }
+
+// export async function storeOdAuthTokens({
+//   accessToken,
+//   accessTokenExpiry,
+//   refreshToken,
+// }: {
+//   accessToken: string
+//   accessTokenExpiry: number
+//   refreshToken: string
+// }): Promise<void> {
+//   await kv.set(`${siteConfig.kvPrefix}access_token`, accessToken, 'EX', accessTokenExpiry)
+//   await kv.set(`${siteConfig.kvPrefix}refresh_token`, refreshToken)
+// }
+
+
+// store token in browser session
+
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
+  // 尝试从浏览器会话中获取缓存的令牌
+  const cachedAccessToken = sessionStorage.getItem('accessToken')
+  const cachedRefreshToken = sessionStorage.getItem('refreshToken')
+
+  // 如果缓存存在，则直接返回缓存的令牌
+  if (cachedAccessToken && cachedRefreshToken) {
+    return {
+      accessToken: cachedAccessToken,
+      refreshToken: cachedRefreshToken,
+    }
+  }
+
+  // 如果缓存不存在，则从 Redis 获取令牌
   const accessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
   const refreshToken = await kv.get(`${siteConfig.kvPrefix}refresh_token`)
+
+  // 将获取的令牌缓存到浏览器会话中
+  sessionStorage.setItem('accessToken', accessToken)
+  sessionStorage.setItem('refreshToken', refreshToken)
 
   return {
     accessToken,
@@ -15,6 +58,7 @@ export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refresh
   }
 }
 
+// 将访问令牌和刷新令牌存储到 Redis，并更新浏览器会话中的缓存
 export async function storeOdAuthTokens({
   accessToken,
   accessTokenExpiry,
@@ -24,6 +68,12 @@ export async function storeOdAuthTokens({
   accessTokenExpiry: number
   refreshToken: string
 }): Promise<void> {
+  // 将令牌存储到 Redis
   await kv.set(`${siteConfig.kvPrefix}access_token`, accessToken, 'EX', accessTokenExpiry)
   await kv.set(`${siteConfig.kvPrefix}refresh_token`, refreshToken)
+
+  // 更新浏览器会话中的缓存
+  sessionStorage.setItem('accessToken', accessToken)
+  sessionStorage.setItem('refreshToken', refreshToken)
 }
+
